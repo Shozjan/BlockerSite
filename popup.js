866d3ec4415c0be5d3a1e56555517e438dc5d9a0
Simon Hozjan <simonh@staninvest.si>
2019-02-25 14:35:56 +0100

    initial commit

---
+++ popup.js
@@ -0,0 +1,61 @@
+
+var sec = 0;
+
+$(document).ready(function(){
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+}, function(tabs) {
+    var link=document.createElement('a');
+     link.href= tabs[0].url;
+  
+    $("#obiskana").html("Trenutno: "+link.host);
+
+     //dodan listener za spremembo tab-a v statistiko
+        var elStats = document.getElementById('toStatistics');
+        if(elStats){
+          elStats.addEventListener('click', spremeniStatiskika, false);
+        }
+
+        //dodan listener za spremembo tab-a v prvotno
+        var elStran = document.getElementById('toStran');
+        if(elStran){
+          elStran.addEventListener('click', spremeniVpodatke, false);
+        }
+
+
+    
+});
+});
+
+function timer(){
+  var timer = setInterval(function(){
+      document.getElementById('cas').innerHTML=sec+" sekund";
+      sec++;    
+  }, 1000);
+}
+
+
+chrome.storage.local.set({"cas": "2"}, function() {
+
+});
+
+chrome.storage.sync.set({'cas': "2"}, function() {
+  console.log("Uspesno");
+});
+
+chrome.storage.local.get(['cas'], function(result) {
+  console.log('Value currently is ' + result.cas);
+});
+ 
+
+
+function spremeniStatiskika(){
+  window.location.href='statistika.html';
+}
+
+function spremeniVpodatke(){
+  window.location.href='popup.html';
+}
+
+
